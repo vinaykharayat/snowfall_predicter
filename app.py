392b614b60dba8519db650661ee7a21189915b79
vinay kharayat <vinaykharayat@gmail.com>
2022-01-08 08:22:46 +0530

    Added Test API call to predict

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
-
+import pandas as pd
+import pickle
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #  json_ = request.json
+    #  query_df = pd.DataFrame(json_)
+    #  query = pd.get_dummies(query_df)
     
-# if __name__ == "__main__":
-#     app.run(debug=True)
+     loaded_model = pickle.load(open('Pickle/xgboost.pkl', 'rb'))
+     return jsonify({'prediction': 'success'})
+
+if __name__ == "__main__":
+    app.run(port=8080)
+
