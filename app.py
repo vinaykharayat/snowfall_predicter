8bf393330f47cb0091ea14a67f601e2e59a08cd6
vinay kharayat <vinaykharayat@gmail.com>
2022-01-09 12:47:13 +0530

    Test: Fix path of the pickle model

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, jsonify
 import pandas as pd
 import pickle
+from pathlib import Path
 app = Flask(__name__)
 
+root = Path(".")
+picklePath = root / "Pickle" / "xgboost.pkl"
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -13,8 +16,8 @@ def predict():
     #  query_df = pd.DataFrame(json_)
     #  query = pd.get_dummies(query_df)
     
-     loaded_model = pickle.load(open('Pickle/xgboost.pkl', 'rb'))
-     return jsonify({'prediction': 'success'})
+    loaded_model = pickle.load(open(picklePath, 'rb'))
+    return jsonify({'prediction': 'success', 'rootPath': root})
 
 if __name__ == "__main__":
     app.run(port=8080)
