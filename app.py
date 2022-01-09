0abf90f9da6d3168bfe8dfc6297e93d969aa7386
vinay kharayat <vinaykharayat@gmail.com>
2022-01-09 14:05:42 +0530

    Fix model error

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ def home():
     return render_template("index.html")
 
 @app.route('/predict', methods=['POST'])
+
 def predict():
     #  json_ = request.json
     #  query_df = pd.DataFrame(json_)
     #  query = pd.get_dummies(query_df)
     
-    loaded_model = pickle.load(open(os.getcwd() + "/Pickle/xgboost.pkl", 'rb'))
+    # loaded_model = pickle.load(open(os.getcwd() + "/Pickle/xgboost.pkl", 'rb'))
+    with open('./Pickle/xgboost.pkl','rb') as f:
+        xgb=pickle.load(f)
     return jsonify({'prediction': 'success'})
 
 
