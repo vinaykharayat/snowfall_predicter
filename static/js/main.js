d6210b6f73ae3d77e23d4def4a2746ee4f7b5c40
vinay kharayat <vinaykharayat@gmail.com>
2022-01-13 08:37:50 +0530

    Design fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,42 @@
-// let url = 'http://127.0.0.1:5000/predict';
-let url = 'https://snowfallpredicter.azurewebsites.net/predict'
+let url = 'http://127.0.0.1:5000/predict';
+// let url = 'https://snowfallpredicter.azurewebsites.net/predict'
 
 let apiKey = "e43f7d5b3cc56c04e4ae9d3d593f7940";
 let openweatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Pithoragarh&cnt=1&appid=" + apiKey;
 
-$.ajax({
-    type: 'post',
-    dataType: 'json',
-    url: openweatherUrl,
-    success: function(data) {
-        console.log(data);
-        let nextPredictionTime = document.getElementById("nextPredictionTime");
+// $.ajax({
+//     type: 'post',
+//     dataType: 'json',
+//     url: openweatherUrl,
+//     success: function(data) {
+//         console.log(data);
+//         let nextPredictionTime = document.getElementById("nextPredictionTime");
 
-        predictionTime = new Date(data.list[0].dt * 1000);
-        nextPredictionTime.innerText = predictionTime;
+//         predictionTime = new Date(data.list[0].dt * 1000);
+//         nextPredictionTime.innerText = predictionTime;
 
-        let nightHours = (data.city.sunset - data.city.sunrise) / 3600;
+//         let nightHours = (data.city.sunset - data.city.sunrise) / 3600;
 
-        let rain = (data.list[0].rain !== undefined) ? data.list[0].rain["3h"] : 0;
+//         let rain = (data.list[0].rain !== undefined) ? data.list[0].rain["3h"] : 0;
 
-        let weatherData = {
-            "maxtempC": data.list[0].main.temp_max,
-            "FeelsLikeC": data.list[0].main.feels_like,
-            "cloudcover": data.list[0].clouds.all,
-            "humidity": data.list[0].main.humidity,
-            "precipMM": rain,
-            "visibility": data.list[0].visibility,
-            "winddirDegree": data.list[0].wind.deg,
-            "windspeedKmph": data.list[0].wind.speed,
-            "nightHours": nightHours
-        }
+//         let weatherData = {
+//             "maxtempC": data.list[0].main.temp_max,
+//             "FeelsLikeC": data.list[0].main.feels_like,
+//             "cloudcover": data.list[0].clouds.all,
+//             "humidity": data.list[0].main.humidity,
+//             "precipMM": rain,
+//             "visibility": data.list[0].visibility,
+//             "winddirDegree": data.list[0].wind.deg,
+//             "windspeedKmph": data.list[0].wind.speed,
+//             "nightHours": nightHours
+//         }
 
-        predict(weatherData);
-    },
-    error: function(xhr, status, error) {
-        alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-    }
-});
+//         predict(weatherData);
+//     },
+//     error: function(xhr, status, error) {
+//         alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+//     }
+// });
 
 function predict(postData) {
     $.ajax({
--- static/js/movingEyes.js
+++ static/js/movingEyes.js
@@ -3,7 +3,7 @@ $("#move-area").mousemove(function(event) {
     var x = (eye.offset().left) + (eye.width() / 2);
     var y = (eye.offset().top) + (eye.height() / 2);
     var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    var rot = (rad * (180 / Math.PI) * -1) + 235;
     eye.css({
         '-webkit-transform': 'rotate(' + rot + 'deg)',
         '-moz-transform': 'rotate(' + rot + 'deg)',
